c87495824be48fd00d18eeb0b86286502c774470
Mauricio Galo <ogalo96@gmail.com>
2016-04-27 23:11:39 -0600

    recibir solicitud

--- simuladorBancos/src/application/ControladorBancos.java
+++ simuladorBancos/src/application/ControladorBancos.java
@@ -1,5 +1,102 @@
 package application;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControladorBancos {
-	
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+
+public class ControladorBancos implements Initializable{
+	private boolean finalizo = false;
+	private ServerSocket servidor;
+
+	@FXML private TextArea txtMensaje;
+	@FXML private TextArea txtResultado;
+	@FXML private ListView<Socket> lstClientes;
+	private ObservableList<Socket> socketsClientes;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		socketsClientes = FXCollections.observableArrayList();
+		lstClientes.setItems(socketsClientes);
+		Task<String> tareaEscucharPeticiones = new Task<String>(){
+			public String call(){
+				try {
+					servidor = new ServerSocket(60666);
+					while(!finalizo){
+						System.out.println("Esperando cliente");
+						Socket cliente= servidor.accept();
+						System.out.println("Cliente conectado: " + cliente);
+						Platform.runLater(new Runnable(){
+							public void run(){
+								socketsClientes.add(cliente);
+							}
+						});
+						procesarMensajesCliente(cliente);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return "Tarea finalizada";
+			}
+		};
+		Thread hilo = new Thread(tareaEscucharPeticiones);
+		hilo.setDaemon(true);
+		hilo.start();
+	}
+
+	public void procesarMensajesCliente(Socket cliente){
+		Task<String> tareaLecturaMensajes = new Task<String>(){
+
+			@Override
+			protected String call() throws Exception {
+				DataInputStream entrada = new DataInputStream(cliente.getInputStream());
+				while(!finalizo){
+					System.out.println("Esperando a que el cliente escriba algo");
+					String mensaje = entrada.readUTF();
+					Platform.runLater(new Runnable(){
+						@Override
+						public void run() {
+								txtResultado.appendText(mensaje+"\n");
+						}
+					});
+				}
+				servidor.close();
+				cliente.close();
+				return "Finalizo la tarea";
+			}
+
+		};
+		Thread hilo = new Thread(tareaLecturaMensajes);
+		hilo.setDaemon(true); //Para que el hilo de la aplicacion
+		//finalicen cuando se cierra la aplicacion
+		hilo.start();//Inicia el hilo, es decir llama al metodo call()
+	}
+
+	@FXML
+	public void enviarMensaje(){
+		 try {
+			Socket clienteSeleccionado =
+					lstClientes.getSelectionModel().getSelectedItem();
+			DataOutputStream salida = new DataOutputStream(clienteSeleccionado.getOutputStream());
+			salida.writeUTF("[SERVIDOR]: " + txtMensaje.getText());
+			txtResultado.appendText("[SERVIDOR]: " + txtMensaje.getText()+"\n");
+			txtMensaje.setText(null);
+			//salida.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- simuladorBancos/src/application/Main.java
+++ simuladorBancos/src/application/Main.java
@@ -1,8 +1,9 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,16 +12,17 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Bancos.fxml"));
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Bancos.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Servidor de transacciones");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
